ab3efeee532a9ba05db4cb8b6bb60095572ee458
alibauez <alibauez@gmail.com>
2024-02-13 11:03:41 +0100

    Primercambio

---
+++ app/Http/Controllers/VeterinarioController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreveterinarioRequest;
+use App\Http\Requests\UpdateveterinarioRequest;
+use App\Models\veterinario;
+
+class VeterinarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreveterinarioRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(veterinario $veterinario)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(veterinario $veterinario)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateveterinarioRequest $request, veterinario $veterinario)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(veterinario $veterinario)
+    {
+        //
+    }
+}
