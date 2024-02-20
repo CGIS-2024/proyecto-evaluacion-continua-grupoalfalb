2e8fe4d77f0fcd8ab653b145bbddc3bccd703a7d
alibauez <alibauez@gmail.com>
2024-02-20 11:14:11 +0100

    encargado_almacen

---
+++ app/Http/Controllers/EncargadoAlmacenController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Storeencargado_almacenRequest;
+use App\Http\Requests\Updateencargado_almacenRequest;
+use App\Models\encargado_almacen;
+
+class EncargadoAlmacenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Storeencargado_almacenRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(encargado_almacen $encargado_almacen)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(encargado_almacen $encargado_almacen)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Updateencargado_almacenRequest $request, encargado_almacen $encargado_almacen)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(encargado_almacen $encargado_almacen)
+    {
+        //
+    }
+}
