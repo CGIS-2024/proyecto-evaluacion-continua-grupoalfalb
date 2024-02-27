a8087e1ec4e22d7583c4598673a58076db5678c7
alibauez <alibauez@gmail.com>
2024-02-27 12:02:30 +0100

    cosassss

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('fecha_nacimiento');
+            $table->string('dni');
+            $table->string('direccion');
+            $table->string('genero');
             $table->rememberToken();
             $table->timestamps();
         });
