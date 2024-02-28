85fb1f3733f924e23b25a6bdcd6e03cf2f0654c0
alibauez <alibauez@gmail.com>
2024-02-28 18:14:32 +0100

    añadido modelo plato

---
+++ app/Http/Controllers/PlatoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePlatoRequest;
+use App\Http\Requests\UpdatePlatoRequest;
+use App\Models\Plato;
+
+class PlatoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePlatoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Plato $plato)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Plato $plato)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePlatoRequest $request, Plato $plato)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Plato $plato)
+    {
+        //
+    }
+}
