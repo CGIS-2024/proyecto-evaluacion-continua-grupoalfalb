9b1c7a238137680b59a668672d8bc0b2094b2344
alibauez <alibauez@gmail.com>
2024-03-16 20:23:07 +0100

    vista

--- resources/views/menus/index.blade.php
+++ resources/views/menus/index.blade.php
@@ -1,24 +1,24 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Citas') }}
+            {{ __('Menus') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                {{--
                 <div class="flex items-center mt-4 ml-2">
                     <form method="GET" action="{{ route('citas.create') }}">
-                       <!--
                         <x-primary-button type="subit" class="ml-4">
                             @if(Auth::user()->es_paciente)
                                 {{ __('Pedir nueva cita') }}
                             @endif
                         </x-primary-button>
-                        -->
+
                     </form>
-                </div>
+                </div> --}}
                 <div class="p-6 bg-white border-b border-gray-200">
                     <table class="min-w-max w-full table-auto">
                         <thead>
