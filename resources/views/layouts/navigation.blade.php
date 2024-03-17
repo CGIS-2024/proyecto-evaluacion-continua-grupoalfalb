5ed5e82e4045fcf512d3ec1a1158e9f321cac2a9
alibauez <alibauez@gmail.com>
2024-03-17 12:50:23 +0100

    policies menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('menus.index')" :active="request()->routeIs('menus.index')">
+                        {{ __('Menús') }}
+                    </x-nav-link>
                 </div>
             </div>
 
