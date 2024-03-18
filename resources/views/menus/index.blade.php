7bd8640a08da89b405d3e235c999d25af763bf67
alibauez <alibauez@gmail.com>
2024-03-18 22:32:53 +0100

    vista index menu

--- resources/views/menus/index.blade.php
+++ resources/views/menus/index.blade.php
@@ -1,38 +1,51 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Menús') }}
+            {{ __('Menus') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="flex items-center mt-4 ml-2">
+                    <form method="GET" action="{{ route('menus.create') }}">
+                        <x-primary-button type="subit" class="ml-4">
+                            @if(Auth::user()->es_dietista)
+                                {{ __('Crear Menu') }}
+                            @endif
+                        </x-primary-button>
+                    </form>
+                </div>
                 <div class="p-6 bg-white border-b border-gray-200">
                     <table class="min-w-max w-full table-auto">
                         <thead>
-                            <tr class="bg-gray-200 text-gray-900 uppercase text-sm leading-normal">
-                                <th class="py-3 px-6 text-left">{{ __('Instrucciones Específicas') }}</th>
-                                <th class="py-3 px-6 text-left">{{ __('Fecha') }}</th>
-                            </tr>
+                        <tr class="bg-gray-200 text-gray-900 uppercase text-sm leading-normal">
+                            <th class="py-3 px-6 text-left">instrucciones_especificas</th>
+                            <th class="py-3 px-6 text-left">fecha</th>
+                        </tr>
                         </thead>
                         <tbody class="text-gray-600 text-sm font-light">
-                            @foreach ($menus as $menu)
-                            <tr class="border-b border-gray-200 hover:bg-gray-100">
+
+                        @foreach ($menus as $menu)
+                            <tr class="border-b border-gray-200">
                                 <td class="py-3 px-6 text-left whitespace-nowrap">
                                     <div class="flex items-center">
-                                        <span class="font-medium">{{ $menu->instrucciones_especificas }}</span>
+                                        <span class="font-medium">{{$menu->instrucciones_especificas}}</span>
                                     </div>
                                 </td>
                                 <td class="py-3 px-6 text-left whitespace-nowrap">
                                     <div class="flex items-center">
-                                        <span class="font-medium">{{ $menu->fecha }}</span>
+                                        <span class="font-medium">{{$menu->fecha}}</span>
                                     </div>
                                 </td>
+
                             </tr>
-                            @endforeach
+                        @endforeach
+
                         </tbody>
                     </table>
+                    {{ $menus->links() }}
                 </div>
             </div>
         </div>
