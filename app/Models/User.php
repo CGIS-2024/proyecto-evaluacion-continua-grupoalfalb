538855ccc27c2767545d5eef03174f57ff38ad3e
alibauez <alibauez@gmail.com>
2024-03-19 10:13:15 +0100

    prueba

--- app/Models/User.php
+++ app/Models/User.php
@@ -68,29 +68,30 @@ class User extends Authenticatable
         if ($this->dietista()->exists()){
             return 1;
         }
-        elseif($this->paciente()->exists()){
+        else {
             return 2;
         }
-        else{
-            return 3;
-        }
+       // else{
+         //   return 3;
+        //}
     }
 
     public function getTipoUsuarioAttribute(){
-        $tipos_usuario = [1 => trans('Dietista'), 2 => trans('Paciente'), 3 => trans('Administrador')];
+        $tipos_usuario = [1 => trans('Dietista'), 2 => trans('Administrador')];
+        //, 3 => trans('Administrador')
         return $tipos_usuario[$this->tipo_usuario_id];
     }
 
-    public function getEsPacienteAttribute(){
-        return $this->tipo_usuario_id == 2;
-    }
+    //public function getEsPacienteAttribute(){
+      //  return $this->tipo_usuario_id == 2;
+    //}
 
     public function getEsDietistaAttribute(){
         return $this->tipo_usuario_id == 1;
     }
 
     public function getEsAdministradorAttribute(){
-        return $this->tipo_usuario_id == 3;
+        return $this->tipo_usuario_id == 2;
     }
 
 
