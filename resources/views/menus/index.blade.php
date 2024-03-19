5287a128e4e5b19dbe6f6c9347c5c56c517efa0d
albgarmar2502 <albertogmpv2502@gmail.com>
2024-03-19 10:49:16 +0100

    Edit view 4

--- resources/views/menus/index.blade.php
+++ resources/views/menus/index.blade.php
@@ -43,13 +43,19 @@
                                         <span class="font-medium">{{$menu->fecha}}</span>
                                     </div>
                                 </td>
-                                <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                <td class="py-3 px-6 text-center">
+                                    <div class="flex item-center justify-end">
+                                        <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
                                             <a href="{{route('menus.edit', $menu->id)}}">
                                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                                                 </svg>
                                             </a>
                                         </div>
+                                    </div>
+                                </td>
+                                    
+                                
 
 
                             </tr>
