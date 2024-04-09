e1ddf239a805f6626bc549be99c30d9346822a8e
alibauez <alibauez@gmail.com>
2024-04-09 12:03:24 +0200

    AUTH SERVICE PROVIDER

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\Dietista;
+use App\Policies\DietistaPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //AQUI ESCRIBO LOS MODELOS QUE QUIERO QUE TENGAN POLICIES
+        Dietista::class => DietistaPolicy::class
     ];
 
     /**
