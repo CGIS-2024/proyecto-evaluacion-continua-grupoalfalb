8e600e08a3b6e10e171ee20783fd1f994a4c0bd4
alibauez <alibauez@gmail.com>
2024-04-09 09:55:04 +0200

    AUTH SERVICE PROVIDER

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        //AQUI ESCRIBO LOS MODELOS QUE QUIERO QUE TENGAN POLICIES
     ];
 
     /**
