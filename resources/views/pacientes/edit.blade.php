5ae76700ad21069ceb71282539388d113d3d0e0e
albgarmar2502 <albertogmpv2502@gmail.com>
2024-04-23 21:19:46 +0200

    PAPELERA EDIT

--- resources/views/pacientes/edit.blade.php
+++ resources/views/pacientes/edit.blade.php
@@ -205,28 +205,30 @@
                                             class="font-medium">{{$menu->instrucciones_especificas}}</span>
                                     </div>
                                 </td>
-
-                                    <div class="flex item-center justify-center">
-
+                                <td class="py-3 px-6 text-left whitespace-nowrap">
+                                    
+                                </td>
+                                <td class="py-3 px-6 text-center whitespace-nowrap">
+                                    <div class="flex items-center justify-center">
                                         <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                                            <form id="detach-form-{{$paciente->id}}-{{$menu->id}}" method="POST"
-                                                  action="{{ route('pacientes.detachMenu', [$paciente->id, $menu->id]) }}">
-                                                @csrf
-                                                @method('delete')
-                                                <a class="cursor-pointer"
-                                                   onclick="getElementById('detach-form-{{$paciente->id}}-{{$menu->id}}').submit();">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                         viewBox="0 0 24 24" stroke="currentColor">
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                              stroke-width="2"
-                                                              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
-                                                    </svg>
-                                                </a>
-                                            </form>
-
-                                        </div>
+                                                <form id="delete-form-{{$paciente->id}}" method="POST" action="{{ route('pacientes.destroy', $paciente->id) }}">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <a class="cursor-pointer" onclick="getElementById('delete-form-{{$paciente->id}}').submit();">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                        </svg>
+                                                    </a>
+                                                </form>
+                                            </div>
                                     </div>
                                 </td>
+
+                                
+
+
+                                    
+                                
                             </tr>
                         @endforeach
 
