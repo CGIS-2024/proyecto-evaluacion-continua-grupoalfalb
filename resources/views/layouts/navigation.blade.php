d36ecb58b8135d15b4effeb625fbef07ec4790d9
albgarmar2502 <albertogmpv2502@gmail.com>
2024-04-30 10:54:26 +0200

    PACIENTES ASOCIADOS A DIETISTA

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,11 +10,12 @@
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
+                
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Inicio') }}
                     </x-nav-link>
+                    
                     @if(Auth::user()->es_administrador || Auth::user()->es_dietista)
                         <x-nav-link :href="route('pacientes.index')" :active="request()->routeIs('pacientes.index')">
                             {{ __('Pacientes') }}
