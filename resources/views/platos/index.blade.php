3af23ea74e34adc59739f4d7730399b54104c723
alibauez <alibauez@gmail.com>
2024-05-11 20:48:48 +0200

    aa

--- resources/views/platos/index.blade.php
+++ resources/views/platos/index.blade.php
@@ -42,7 +42,7 @@
                                 </td>
                                 <td class="py-3 px-6 text-left whitespace-nowrap">
                                     <div class="flex items-center">
-                                        <span class="font-medium">{{$plato->pivot->categoriaplato_id->nombre}}</span>
+                                        <span class="font-medium">{{$plato->pivot}}</span>
                                     </div>
                                 </td>
                                 <td class="py-3 px-6 text-left whitespace-nowrap">
