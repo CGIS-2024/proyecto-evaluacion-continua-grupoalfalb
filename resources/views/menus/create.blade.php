306278caf5904a2cc09c5f23b74492602b382436
albgarmar2502 <albertogmpv2502@gmail.com>
2024-05-14 10:07:27 +0200

    COMMIT

--- resources/views/menus/create.blade.php
+++ resources/views/menus/create.blade.php
@@ -35,7 +35,7 @@
                         </div>
                         @if(Auth::user()->es_administrador)
                             <div class="mt-4">
-                                <x-select id="dietista_id" name="Dietista" required>
+                                <x-select id="dietista_id" name="dietista_id" required>
                                     <option value="">{{__('Elige un dietista')}}</option>
                                     @foreach ($dietistas as $dietista)
                                         <option value="{{$dietista->id}}" @if (old('dietista_id') == $dietista->id) selected @endif>{{$dietista->user->name}} </option>
