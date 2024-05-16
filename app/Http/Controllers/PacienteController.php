967904175f40dddd949e100b7a4e102102cbc74b
albgarmar2502 <albertogmpv2502@gmail.com>
2024-05-16 19:19:58 +0200

    COMMIT PACIENTE

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -18,7 +18,7 @@ class PacienteController extends Controller
     private function createUser(Request $request)
     {
         $user = new User($request->validated());
-        $user->password = Hash::make($user->password);
+        
         $user->save();
         return $user;
     }
